45594c357e82f06f5813d5bb22d774e59a7c4683
2JeongYun <leejeongyun828@gmail.com>
2021-01-25 12:20:54 +0900

    Rename GreedyAlgorithms to greedyAlgorithms

--- src/this_is_coding_test/ch12/Wall.java
+++ src/this_is_coding_test/ch12/Wall.java
@@ -1,25 +1,22 @@
 package this_is_coding_test.ch12;
 
+import java.util.ArrayList;
+
 // Unsolved
 public class Wall {
     public int solution(int n, int[] weak, int[] dist) {
-        int answer = 0;
-
-        int[][] weakDist = new int[weak.length][weak.length];
-        for (int i = 0; i < weak.length; i++) {
-            for (int j = i; j < weak.length; j++) {
-                weakDist[i][j] = weak[j] - weak[i];
-            }
-        }
+        int answer = Integer.MAX_VALUE;
 
         return answer;
     }
 
+    private boolean check()
+
     public static void main(String[] args) {
         Wall w = new Wall();
         int n = 12;
-        int[] weak = {1, 5, 6, 10};
-        int[] dist = {1, 2, 3, 4};
+        int[] weak = {1, 3, 4, 9, 10};
+        int[] dist = {3, 5, 7};
         System.out.println(w.solution(n, weak, dist));
     }
 }
