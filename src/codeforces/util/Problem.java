318ec26e4fbeaca593a97f403855e4433ad378be
2JeongYun <leejeongyun828@gmail.com>
2021-03-11 18:46:06 +0900

    Add Problem template

---
+++ src/codeforces/util/Problem.java
@@ -0,0 +1,23 @@
+package codeforces.util;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Problem {
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        br.close();
+        bw.close();
+    }
+
+    public void solve(BufferedReader br, BufferedWriter bw) throws IOException {
+        StringTokenizer st;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Problem p = new Problem();
+        p.solution();
+    }
+}
